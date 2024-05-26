48abebd41c077f4ff4d81a5d2abec852f6f09c82
Aviv <54484588+avivper@users.noreply.github.com>
2024-05-26 12:41:25 +0300

    Add files via upload

---
+++ EpisodeB/MashovB.java
@@ -0,0 +1,15 @@
+package EpisodeB;
+
+/*
+This file is containing the comments + methods from the Mashov to Episode B
+ */
+
+public class MashovB {
+    public static void main(String[] args) {
+        MashovB1();
+    }
+
+    public static void MashovB1() {
+        System.out.println("MashovB1");
+    }
+}
