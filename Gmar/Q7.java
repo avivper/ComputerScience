c19cfddce5cd0ef51b3f23e67eeec75d48e3da93
Aviv <54484588+avivper@users.noreply.github.com>
2024-08-14 13:50:55 +0300

    Add files via upload

--- Gmar/Q7.java
+++ Gmar/Q7.java
@@ -4,10 +4,59 @@ import java.util.Arrays;
 
 public class Q7 {
     /*
-    7 8 9
-    8 9 0
-    9 0 0
+    מטריצה משופעת:
+    בכל אחד מן האלכסונים היורדים מן השורה העליונה שמאלה ולמטה כל השווים שווים, וכן
+    בכל אחד מן האלכסונים היורדים מן העמודה הימנית שמאלה ולמטה כל האיברים שווים
      */
+    public static void main(String[] args) {
+        int[][] a = new int[][]{ // Left Matrix
+                {7, 8, 7},
+                {7, 7, 8},
+                {7, 8, 7}
+        };
+
+        int[][] b = new int[][]{  // Middle Matrix
+                {1, 2, 3, 4},
+                {5, 1, 2, 3},
+                {6, 5, 1, 2},
+        };
+
+        int[][] c = new int[][]{
+                {1, 2, 3, 4, 5, 6},
+                {2, 3, 4, 5, 6, 7},
+                {3, 4, 5, 6, 7, 8},
+                {4, 5, 6, 7, 8, 0},
+        };
+
+        System.out.println(isSlopeMatrix(a) + "\n" +
+                isSlopeMatrix(b) + "\n" +
+                isSlopeMatrix(c));
+    }
+    
+    /*
+    The Method will convert the Middle Matrix b to:
+    {1,2,3,4},
+    {2,3,4,3},
+    {3,4,3,2}
+     */
+
+    // Question A
+    public static void sloppingMatrix(int[][] a) {
+        for (int i = 0; i < a[0].length-1; i++) {
+            if (a[0][i]+1 != a[0][i+1]) {
+                a[0][i+1] = a[0][i]+1;
+            }
+        }
+
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a[i].length-1; j++) {
+                if (a[i][j] != a[i-1][j+1]) {
+                    a[i][j] = a[i-1][j+1];
+                }
+            }
+        }
+    }
+
     public static void printArray(int[][] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
@@ -17,9 +66,23 @@ public class Q7 {
         }
     }
 
-    public static void main(String[] args) {
-        int[][] a = new int[][]{{7, 8, 7}, {7, 7, 8}, {7, 8, 7}};
-        // printArray(a);
-        // sloppingMatrix(a);
+    // Question B
+    public static boolean isSlopeMatrix(int[][] a) {
+        for (int i = 0; i < a.length-1; i++) {
+            if (a[i][i]+1 != a[i+1][i]) {
+                return false;
+            }
+        }
+
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a[i].length-1; j++) {
+                if (a[i][j] != a[i-1][j+1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
     }
 }
